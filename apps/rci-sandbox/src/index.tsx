51e036aaea4f90de1b47ac30c895e4f7dce595a6
Leonardo Dino <leonardodino@users.noreply.github.com>
2023-03-25 22:57:39 +0000

    :recycle: use tailwind for styling the sandbox (#28)

--- apps/rci-sandbox/src/index.tsx
+++ apps/rci-sandbox/src/index.tsx
@@ -1,23 +1,21 @@
-import 'modern-normalize'
 import './index.css'
 import { createRoot } from 'react-dom/client'
 import { Example } from './Example'
 
-const expected = '123123'
-const a11yId = 'code-input'
-
-const Page = () => (
+const Page = ({ expected = '123123', a11yId = 'code-input' }) => (
   <div>
-    <h1>
+    <h1 className='my-6 text-[2em] font-bold'>
       <code>rci</code> example
     </h1>
-    <label htmlFor={a11yId}>
-      authentication code&emsp;<span>expected: {expected}</span>
+    <label htmlFor={a11yId} className='block pb-2.5'>
+      authentication code&emsp;
+      <span className='opacity-50'>expected: {expected}</span>
     </label>
     <Example id={a11yId} expected={expected} />
     <a
       href='https://github.com/leonardodino/rci'
       title='leonardodino/rci'
+      className='text-current no-underline'
       style={{ display: 'block', padding: '1rem 0', margin: '2rem 0' }}
     >
       <code>rci</code> @ GitHub
