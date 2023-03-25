a5e623ea67c4a2ffa90fec9e79695eb9ca5ed989
Leonardo Dino <leonardo@kunstdesign.com.br>
2023-03-25 03:14:02 +0000

    :heavy_plus_sign: install tailwind & prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'all',
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
